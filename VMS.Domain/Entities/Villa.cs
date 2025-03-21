a87e5e5aee548d54bee367e34eda671a4d99dcf8
Shubham Verma <brisbineheat88@gmail.com>
2025-03-21 15:46:26 +0530

    Toaster work

--- VMS.Domain/Entities/Villa.cs
+++ VMS.Domain/Entities/Villa.cs
@@ -11,11 +11,17 @@ namespace VMS.Domain.Entities
     {
         [Key]
         public int ID { get; set; }
+        [MaxLength(50)]
         public required string Name {  get; set; }
         public string? Description { get; set; }
+        [Display(Name="Price Per Night")]
+        [Range(10,10000)]
         public double Price { get; set; }
+
         public int Sqft { get; set; }
+        [Range(1,10)]
         public int Occupancy { get; set; }
+        [Display(Name="Image Url")]
         public string? ImageUrl { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get;set; }
--- VMS.Web/Controllers/VillaController.cs
+++ VMS.Web/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VMS.Domain.Entities;
 using VMS.Infrastructure.Data;
 
 namespace VMS.Web.Controllers
@@ -14,6 +15,83 @@ namespace VMS.Web.Controllers
         {
             var data = _context.Villas.ToList();
             return View(data);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if (villa.Name == villa.Description)
+            {
+                ModelState.AddModelError("description", "Name And Description Are Not Exactly Same");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Add(villa);
+                _context.SaveChanges();
+                TempData["success"] = "Data Has Been Created Successfully";
+
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "Error Occured";
+            return View();
+       
+        }
+        public IActionResult Edit (int Villaid)
+        {
+            Villa? Data = _context.Villas.FirstOrDefault(u=>u.ID==Villaid);
+            if(Data == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(Data);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Villa villa)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Update(villa);
+                _context.SaveChanges();
+                TempData["success"] = "Data Has Been Updated Successfully";
+
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "Error Occured";
+            return View();
+        
+
+        }
+
+        public IActionResult Delete(int Villaid)
+        {
+            Villa? data = _context.Villas.FirstOrDefault(v => v.ID==Villaid);
+            if (data == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+          return View(data) ;
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa villa)
+        {
+            Villa? delvilla = _context.Villas.FirstOrDefault(u => u.ID == villa.ID);
+            if(delvilla is not null)
+            {
+                _context.Remove(delvilla);
+                _context.SaveChanges();
+                TempData["success"] = "Data Has Been Deleted Successfully";
+                return RedirectToAction("Index");
+
+            }
+            TempData["error"] = "Error Occured";
+            return View();
         }
     }
 }
